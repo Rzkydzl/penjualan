02c50fbf48146ed8e6b77dacf665d5f4896a49fb
Rizky Dzulsaadya Sudrajat <rizky.dzulsaadya@gmail.com>
2023-12-09 17:06:53 +0700

    first commit

---
+++ application/views/admin/footer.php
@@ -0,0 +1,8 @@
+		<footer class="footer">
+			<ul class="list-inline">
+				<li>2023 © <i>Jasa</i>-<b>Desain</b>.</li>
+				<li><a href="#">Privacy</a></li>
+				<li><a href="#">Terms</a></li>
+				<li><a href="#">Help</a></li>
+			</ul>
+		</footer>
---
+++ application/views/home/main.php
@@ -0,0 +1,169 @@
+    <section class="section ft-feature-1">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-12 bg-black w-100 ft-feature-1-content">
+            <div class="row align-items-center">
+              <div class="col-lg-5">
+                <div class="h-100">
+                  <div class="mb-5 d-flex align-items-center">
+                    <!-- <div>
+                      <a href="https://vimeo.com/317571768" class="popup-vimeo d-block play"><span class="icon-play"></span></a>
+                    </div> -->
+                    <h2>Kami Menyiadiakan Solusi Untuk Hasil Desain Yang Lebih Baik</h2>
+                  </div>
+                  <img src="<?=base_url()?>images/about_1.jpg" alt="Image" class="img-fluid">
+                </div>
+              </div>
+              <div class="col-lg-3 ml-auto">
+                <div class="mb-5">
+                  <h3 class="d-flex align-items-center"><span class="icon icon-phonelink mr-2"></span><span>Spanduk</span></h3>
+									<p>Pilihan Terbaik Untuk Anda.</p>
+                  <p><a href="<?=base_url()?>home/project">Lihat Desain</a></p>
+                </div>
+
+                <div>
+                  <h3 class="d-flex align-items-center"><span class="icon icon-extension mr-2"></span><span>Kartu Nama</span></h3>
+                  <p>Pilihan Terbaik Untuk Anda.</p>
+                  <p><a href="<?=base_url()?>home/project">Lihat Desain</a></p>
+                </div>
+
+              </div>
+              <div class="col-lg-3">
+                <div class="mb-5">
+                  <h3 class="d-flex align-items-center"><span class="icon icon-format_paint mr-2"></span><span>Undangan</span></h3>
+                  <p>Pilihan Terbaik Untuk Anda.</p>
+                  <p><a href="<?=base_url()?>home/project">Lihat Desain</a></p>
+                </div>
+
+                <div>
+                 <!--  <h3 class="d-flex align-items-center"><span class="icon icon-question_answer mr-2"></span><span>Copywriting</span></h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ab nihil quam nesciunt.</p>
+                  <p><a href="#">Read More</a></p> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+   
+
+    <section class="site-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-md-8 text-center">
+            <h2 class="text-black h1 site-section-heading text-center">Desain Project</h2>
+            <p class="lead">Mengapa Harus Kami</p>
+						<p>Solusi digital terpadu dengan jaminan Desain Yang Baik Dan Menarik</p>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6 col-lg-4">
+            <a href="<?=base_url()?>home/project" class="media-1">
+              <img src="<?=base_url()?>images/produk/24.jpg" alt="Image" class="img-fluid">
+              <div class="media-1-content">
+                <h2>Spanduk</h2>
+                <!-- <span class="category">Web Application</span> -->
+              </div>
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <a href="<?=base_url()?>home/project" class="media-1">
+              <img src="<?=base_url()?>images/produk/86.jpg" alt="Image" class="img-fluid">
+              <div class="media-1-content">
+                <h2>Kartu Nama</h2>
+                <!-- <span class="category">Branding</span> -->
+              </div>
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <a href="<?=base_url()?>home/project" class="media-1">
+              <img src="<?=base_url()?>images/produk/69.jpg" alt="Image" class="img-fluid">
+              <div class="media-1-content">
+                <h2>Undangan</h2>
+                <!-- <span class="category">Website</span> -->
+              </div>
+            </a>
+          </div>
+
+          <div class="col-12 text-center mt-5">
+            <a href="<?=base_url()?>home/project" class="btn btn-primary btn-md">Lihat Semua Desain Project</a>
+          </div>
+
+          
+        </div>
+      </div>
+    </section>
+
+
+    <section class="site-section testimonial-wrap">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-8 text-center">
+            <h2 class="text-black h1 site-section-heading text-center">Telah Dipercaya 10.000+ Pelanggan Di Indonesia & Asia. </h2>
+          </div>
+        </div>
+      </div>
+      <div class="slide-one-item home-slider owl-carousel">
+          <div>
+            <div class="testimonial">
+              
+              <blockquote class="mb-5">
+                <p>&ldquo;Saya Mempercayai Website Ini Untuk Solusi Desain Yang Sangat Baik.&rdquo;</p>
+              </blockquote>
+
+              <figure class="mb-4 d-flex align-items-center justify-content-center">
+                <div><img src="<?=base_url()?>images/person_3.jpg" alt="Image" class="w-50 img-fluid mb-3"></div>
+                <p>Andy Rahman</p>
+              </figure>
+            </div>
+          </div>
+          <div>
+            <div class="testimonial">
+
+              <blockquote class="mb-5">
+                <p>&ldquo;Tak Terasa Sudah 1 Tahun Saya Berlanggan Memesan Jasa Desain Di Website Ini.&rdquo;</p>
+              </blockquote>
+              <figure class="mb-4 d-flex align-items-center justify-content-center">
+                <div><img src="<?=base_url()?>images/person_2.jpg" alt="Image" class="w-50 img-fluid mb-3"></div>
+                <p>Takuya Manada</p>
+              </figure>
+              
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+
+              <blockquote class="mb-5">
+                <p>&ldquo;Selain Hasil Desain Yang Sangat Memuaskan Harga Dan Pelayanan nya Sangat Memuaskan Juga.&rdquo;</p>
+              </blockquote>
+              <figure class="mb-4 d-flex align-items-center justify-content-center">
+                <div><img src="<?=base_url()?>images/person_4.jpg" alt="Image" class="w-50 img-fluid mb-3"></div>
+                <p>Rahmad Alif</p>
+              </figure>
+
+              
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+
+              <blockquote class="mb-5">
+                <p>&ldquo;Ini Pertama Kalinya Aku Pesan Undangan Untuk Acara Pernikahan, Dan Hasil Desain nya Aku Suka Banget Dan Harga nya Juga Sangat Terjangkau.&rdquo;</p>
+              </blockquote>
+              <figure class="mb-4 d-flex align-items-center justify-content-center">
+                <div><img src="<?=base_url()?>images/person_5.jpg" alt="Image" class="w-50 img-fluid mb-3"></div>
+                <p>Alifia Ananda</p>
+              </figure>
+
+            </div>
+          </div>
+
+        </div>
+    </section>
